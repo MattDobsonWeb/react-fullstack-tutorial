1c3cdafdbcafe9778a58d4bb5ac4655269530f57
MattDobsonWeb <hello@mattdobson.me>
2021-12-29 21:18:14 +0000

    Ready for deployment

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/api/todos", toDosRoute);
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-  req.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 mongoose
